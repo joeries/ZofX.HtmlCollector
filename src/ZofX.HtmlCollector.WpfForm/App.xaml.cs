2979cf156f3195d399ad4fd400d0d95186589ce7
yangdejiu <yangdejiu@163.com@9043f26a-8f73-40cc-9d63-44b642ae8ca0>
2018-03-21 05:41:25 +0000

    no commit message

---
+++ src/ZofX.HtmlCollector.WpfForm/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace ZofX.HtmlCollector.WpfForm
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            Application.Current.DispatcherUnhandledException += Application_DispatcherUnhandledException;
+            AppDomain.CurrentDomain.UnhandledException += AppDomain_UnhandledException;
+        }
+
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.InnerException == null ? e.Exception.Message : e.Exception.InnerException.Message);
+        }
+
+        private void AppDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Exception ex = e.ExceptionObject as Exception;
+            MessageBox.Show(ex.InnerException == null ? ex.Message : ex.InnerException.Message);
+        }
+    }
+}
